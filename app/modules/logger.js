e8c44b4247306d5a174c13473c88eb2d2c94f1d5
berder <vincent.berder@al-enterprise.com>
2020-01-13 10:27:39 +0100

    npm outdated
    ncu -u
    => rainbow-node-sdk-1.65.2

--- app/modules/logger.js
+++ app/modules/logger.js
@@ -4,22 +4,31 @@ const winston = require("winston");
 const LOG_ID = "STARTER/LOGS - ";
 
 const tsFormat = () => {
-    
         let date = new Date();
-    
         return date.toLocaleDateString() + " " + date.toLocaleTimeString() + " [" + date.valueOf() + "]";
     };
+
+const myFormat = winston.format.printf(info => {
+    //return `${info.timestamp} [${info._processInfo.pid}] ${info.level}: ${info.message}`;
+    return `${tsFormat()} - ${info.level}: ${info.message}`;
+});
 
 class Logger {
 
     constructor() {
 
-        this._logger = new (winston.Logger)({
+        this._logger = winston.createLogger({
+            format: winston.format.combine(
+               // winston.format.colorize({ all: logColor }),
+                winston.format.simple(),
+                //winston.format.label({ label: 'right meow!' }),
+                winston.format.timestamp(),
+                myFormat
+                //winston.format.prettyPrint()
+            ),
             transports: [
-                new (winston.transports.Console)({ 
-                    colorize: true, 
-                    timestamp: tsFormat, 
-                    level: "debug" 
+                new (winston.transports.Console)({
+                    level: "debug"
                 })
             ]
         });
